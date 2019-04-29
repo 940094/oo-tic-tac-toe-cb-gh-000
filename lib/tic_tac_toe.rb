1d2e52a857f56c609cb87c87b4a771a79f3f94f5
Justin Shaw <easyworkemail@gmail.com>
2019-04-29 19:55:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,10 +52,7 @@ class TicTacToe
     end
   end
 
-  def turn_count(board)
-    # Assumes player who goes first is 'X'
-    # When X goes, there is 1 occupied space so it's O's turn. When ) goes, there are 2 occupied spaces, so it's X's turn
-    # iterate through each board and keep count of Xs or Os
+  def turn_count
     count = 0
     board.each do |cell|
       if cell == 'X' || cell == 'O'
